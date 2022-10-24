8aff40f91fd5aa5aaa507fde8fec322e08512ab4
Gary Li <jiaweigaryli@gmail.com>
2022-10-24 08:09:48 -0700

    updated some styling

--- MokuWebsite/MokuWebsite/Models/MenuItems.cs
+++ MokuWebsite/MokuWebsite/Models/MenuItems.cs
@@ -12,7 +12,7 @@ namespace MokuWebsite.Models
         }
     
         public string ItemName { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public string IsSeafood { get; set; }
         public string HasGluten { get; set; }
         
