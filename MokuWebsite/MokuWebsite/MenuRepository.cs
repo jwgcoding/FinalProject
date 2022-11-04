fe15cbba192070d34bd6790131b96e61675126bc
Gary Li <jiaweigaryli@gmail.com>
2022-11-04 12:20:39 -0700

    fixed some layout

--- MokuWebsite/MokuWebsite/MenuRepository.cs
+++ MokuWebsite/MokuWebsite/MenuRepository.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MokuWebsite
 {
-
+    // Connects to database
     public class MenuRepository : IMenuRepository
     {
         private readonly IDbConnection _conn;
@@ -14,37 +14,40 @@ namespace MokuWebsite
         {
             _conn = conn;
         }
-
+        //Grabs all items by categoryid and id
         public IEnumerable<MenuItems> GetAllItems()
         {
             return _conn.Query<MenuItems>("SELECT * FROM MenuItems ORDER BY CategoryID , ID");
         }
 
+        //grabs single item
         public MenuItems GetItem(int id)
         {
             return _conn.QuerySingle<MenuItems>("SELECT * FROM MenuItems WHERE ID = @id ", new { id = id });
 
         }
-
+        // grabs all skewer items by catgoryid
         public IEnumerable<MenuItems> GetAllSkewers()
         {
             return _conn.Query<MenuItems>("SELECT * FROM MenuItems WHERE CategoryID = 1 Order by ID");
         }
-
+        //grabs all tapas item by categoryid
         public IEnumerable<MenuItems> GetAllTapas()
         {
             return _conn.Query<MenuItems>("SELECT * FROM MenuItems WHERE CategoryID = 2 ORDER BY ID");
         }
+        //grabs all ramens by categoryid
         public IEnumerable<MenuItems> GetAllRamen()
         {
             return _conn.Query<MenuItems>("SELECT * FROM MenuItems WHERE CategoryID = 3 ORDER BY ID");
         }
+        //updates  menu items
         public void UpdateMenuItems(MenuItems menuItems)
         {
             _conn.Execute("UPDATE MenuItems SET ItemName = @name, Price = @price, CategoryID = @category WHERE ID = @id",
              new { name = menuItems.ItemName, price = menuItems.Price, category = menuItems.CategoryID, id = menuItems.ID });
         }
-
+        // create new menu items in the menu items
         public void InsertMenuItems(MenuItems menuItems)
         {
             _conn.Execute("INSERT INTO MenuItems (ItemNAME, PRICE, CATEGORYID, HasGluten, IsSeafood) VALUES (@name, @price, @categoryID, @seafood, @gluten);",
@@ -52,13 +55,15 @@ namespace MokuWebsite
 
         }
 
-
+        //assign menuitems to menuitems method 
         public MenuItems AssignMenuItem()
         {
 
             var menuItems = new MenuItems();
             return menuItems;
         }
+
+        //deletes menu items by id
         public void DeleteMenuItems(MenuItems menuItems)
         {
             _conn.Execute("DELETE FROM MenuItems WHERE ID = @id;", new { id = menuItems.ID });
